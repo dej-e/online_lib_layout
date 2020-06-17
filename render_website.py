e2568be9f7b0fa96201c7aaba21a58cec4e5e937
dej-e <developde@mail.ru>
2020-06-17 17:27:54 +0300

    Added some fixes.

--- render_website.py
+++ render_website.py
@@ -1,30 +1,21 @@
+import argparse
 import json
+import math
+import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from livereload import Server, shell
+from livereload import Server
 from more_itertools import chunked
-import math, os
 
 
 def on_reload():
-    #render_index_pages('template.html', books_description, 20)
-    pass
-
-
-def parse_json(filename):
-    return json.load(open(filename, encoding='utf-8'))
+    render_index_pages(template, books_description, step)
+    print("template changed.  reloading.")
 
 
 def render_index_pages(template, books_description, step):
     books_count = len(books_description)
     total_pages = math.ceil(books_count / step)
     page_numbers = list(range(1, total_pages + 1))
-    print(page_numbers)
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html'])
-    )
-    template = env.get_template(template)
 
     folder = 'pages'
     os.makedirs(folder, exist_ok=True)
@@ -45,27 +36,35 @@ def render_index_pages(template, books_description, step):
         page_number += 1
 
 
-def main():
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Library creater (tululu.org)'
+    )
+    parser.add_argument('--books',
+                        default=20,
+                        help='Books per page (default 20)'
+                        )
+    args = parser.parse_args()
+
+    step = int(args.books)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
     try:
-        with open("books_description.json", "r", encoding='utf-8') as my_file:
-            books_description_json = my_file.read()
+        with open("books_description.json", "r",
+                  encoding='utf-8') as book_file:
+            books_description_json = book_file.read()
     except FileNotFoundError:
         print('File "books_description.json" does not exist')
         exit()
     books_description = json.loads(books_description_json)
 
-    render_index_pages('template.html', books_description, 20)
-    live_server = Server()
-    live_server.watch('template.html', on_reload)
+    render_index_pages(template, books_description, step)
 
-    render_index_pages('template.html', books_description, 20)
-    # render_index_page(books_description)
-
-    # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    # server.serve_forever()
-    #live_server.serve(root='pages/.')
-    live_server.serve(root='.')
-
-
-if __name__ == '__main__':
-    main()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.', )
